2168dc7af558a968eb3269c38a8ce58ec27c4646
Ray Jones <ajones@stuy.edu>
2017-02-28 06:29:20 -0500

    making sure its the updated one

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -1,21 +1,33 @@
 public class KnightBoard{
-    private board[][];
+    private int[][]board;
 
-    public KnightBoard(int n){
-	board[][] = new int[n][n]
+    public KnightBoard(int n, int m){
+	board = new int[m][n];
 	
-	    }
-
+    }
+    
+    private void clear(){
+        for(int i = 0; i < board.length; i++){
+            for(int k = 0; k < board.length; k++){
+                board[k][i] = 0;
+            }
+        }
+    }
+    
     public String toString(){
 	String ans = "";
-	for(int col = 0; col<board[row].length;col++){
-	    for(int row = 0; row<board.length;row++){
+	for(int col = 0; col<board.length;col++){
+	    for(int row = 0; row<board[col].length;row++){
 		ans += " ";
-		if (board[row][col] == 0){
+		if (board[col][row] == 0){
 		    ans += "_";
 		}
 		else{
-		    ans += "" += board[col][row];
+		    if(board[col][row] < 10){
+		    ans += " " + board[col][row];
+			
+		    }
+		    else{ans += "" + board[col][row];}
 		}
 	    }
 	    ans += "\n";
@@ -23,83 +35,56 @@ public class KnightBoard{
 	return ans;
     }
 
-    private boolean goodSpace(col, row){
-	return board[col][row] === 0;
+    private boolean goodSpace(int col,int row){
+	return board[col][row] == 0;
 	
     }
 
-    public void addKnight(row, col, id){
+    public void addKnight(int row, int col, int id){
 	board[col][row] = id;
     }
 
-    public void removeKnight(row, col, id){
+    public void removeKnight(int row, int col, int id){
 	board[col][row] = 0;
     }
 
     public boolean solve(){
-	this.SolveH(0);
+	solveH(0,0,1);
 	return true;
     }
- 
-    public boolean solveH(row, col, id){
-	if(done(id)){return true;}
-	if(goodSpace(col, row)){
-	    addKnight(row, col, id);
-
-	    if(board[col+2][row+1] == 0){
-		board[col+2][row+1] = id;
-		solveH(row+1, col+2, id+1)
-		    return true;}
-	    
-	    if(board[col+1][row+2] == 0){
-		board[col+1][row+2] = id;
-		solveH(row+2, col+1, id+1)
-		    return true;}
-	    
-	    if(board[col-2][row-1] == 0){
-		board[col-2][row-1] = id;
-		solveH(row-1, col-2, id+1)
-		    return true;}
-	    
-	    if(board[col-1][row-2] == 0){
-		board[col-1][row-2] = id;
-		solveH(row-2, col-1, id+1)
-		    return true;}
-	    
-	    if(board[col-2][row+1] == 0){
-		board[col-2][row+1] = id;
-		solveH(row+1, col-2, id+1)
-		    return true;}
-	    
-	    if(board[col+2][row-1] == 0){
-		board[col+2][row-1] = id;
-		solveH(row-1, col+2, id+1)
-		    return true;}
-	    
-	    if(board[col+1][row-2] == 0){
-		board[col+1][row-2] = id;
-		solveH(row-2, col+1, id+1)
-		    return true;}
-	    
-	    if(board[col-1][row+2] == 0){
-		board[col-1][row+2] = id;
-		solveH(row+2, col-1, id+1)
-		    return true;}
-	    else{
-	    removeKnight(row, col, id){
-		return false;}
-	    }}
+    private boolean solveH(int row, int col, int id){
+	// are we done?
+	if(id > board.length * board.length){
+            return true;
+        }
+	//index in bounds?
+        if(col >= board.length || col < 0 || row >= board.length || row < 0){
+            return false;
+        }
+        if(board[col][row] == 0){
+            board[col][row] = id;
+            if(solveH(row+2, col+1, id+1) || solveH(row+1, col+2, id+1) || solveH(row-2, col-1, id+1) || 
+	       solveH(row-1, col+2, id+1) || solveH(row-1, col-2, id+1) || solveH(row+2, col-1, id+1) || 
+	       solveH(row-2, col+1, id+1) || solveH(row+1, col-2, id+1)){
+                return true;
+            }
+            board[col][row] = 0;
+        }
+        return false;
     }
     
-
-	public boolean done(row, col, id){
-	    if(id >= row * col){
-		return true
-		    }
-	    else{
-		return false}
+    public boolean done(int row, int col, int id){
+	if(id >= row * col){
+	    return true;
 	}
-	
-	public static void main(String[]args){
-	}
+	else{
+	    return false;}
     }
+	
+    public static void main(String[]args){
+        KnightBoard a = new KnightBoard(5,5);
+        a.solve();
+        System.out.println(a);
+
+    }
+}
